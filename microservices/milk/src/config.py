3c5a03a81e48ff164f3521c5be99a7c967d09731
Dilip Sharma <dilip.sharma@ishafoundation.org>
2023-01-02 20:57:24 +0530

    add default value for db_url

--- microservices/milk/src/config.py
+++ microservices/milk/src/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     aws_dynamo_db:bool = True # default value if env variable does not exist
-    db_url:str
+    db_url:str = ""
     aws_region: str
     aws_access_key_id: str
     aws_secret_access_key: str
