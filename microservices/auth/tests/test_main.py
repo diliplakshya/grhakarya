35227dc56604155ac0ea8d363dfcc6e2625f166c
Dilip Sharma <dilip.sharma@ishafoundation.org>
2023-02-02 17:05:11 +0530

    add poetry environment for dev, test, docs

---
+++ microservices/auth/tests/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from ..src.main import app
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"Samskriti": "Samskar"}
