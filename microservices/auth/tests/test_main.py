b97be3de42a44d691ebfce6c606f54d4eb62ca48
Dilip Sharma <dilip.sharma@ishafoundation.org>
2023-02-12 00:50:03 +0530

    run unit tests

--- microservices/auth/tests/test_main.py
+++ microservices/auth/tests/test_main.py
@@ -1,5 +1,10 @@
 from fastapi.testclient import TestClient
 from fastapi import status
+import os, sys
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__name__), 'src')))
+
 from ..src.main import app
 
 
